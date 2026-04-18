aa01d72e6c34f3c64560b46d5754edfb90285eb4
Abdallahawad3 <djdgdnnxcbj@gmail.com>
2026-04-18 09:53:33 +0200

    Add category model with schema definition for name and slug

---
+++ src/models/categoryModel.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from "mongoose";
+
+const categoryScheme = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: [true, "Category name must be unique"],
+      minLength: [3, "Category name must be at least 3 characters long"],
+      maxLength: [32, "Category name must be at most 32 characters long"],
+    },
+
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const categoryModel = model("Category", categoryScheme);
+
+export default categoryModel;
