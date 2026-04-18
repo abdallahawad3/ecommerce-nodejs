0a138235f414c101e6f8027be82920585ed5ff8d
Abdallahawad3 <djdgdnnxcbj@gmail.com>
2026-04-18 22:45:30 +0200

    feat: add validation for adding subCategories

--- src/routes/subCategoryRoute.ts
+++ src/routes/subCategoryRoute.ts
@@ -1,7 +1,8 @@
+import { ADD_SUBCATEGORY_VALIDATION } from "./../validation/subCategoryValidation.js";
 import { Router } from "express";
 import { createSubCategory, getAllSubCategories } from "../services/subCategoryService.js";
 const router = Router();
 
-router.route("/").post(createSubCategory).get(getAllSubCategories);
+router.route("/").post(ADD_SUBCATEGORY_VALIDATION, createSubCategory).get(getAllSubCategories);
 
 export default router;
---
+++ src/validation/subCategoryValidation.ts
@@ -0,0 +1,38 @@
+import { body, param } from "express-validator";
+import validation from "../middlewares/validation.js";
+
+export const CHECK_ID_VALIDATION = [
+  param("id").isMongoId().withMessage("Invalid category ID format"),
+];
+
+export const ADD_SUBCATEGORY_VALIDATION = [
+  body("name")
+    .notEmpty()
+    .withMessage("SubCategory name is required")
+    .trim()
+    .isLength({ min: 3, max: 32 })
+    .withMessage("Name must be between 3 and 32 characters"),
+  body("category")
+    .notEmpty()
+    .withMessage("Category ID is required")
+    .isMongoId()
+    .withMessage("Invalid category ID format"),
+  validation,
+];
+
+export const UPDATE_SUBCATEGORY_VALIDATION = [
+  body("name")
+    .optional()
+    .notEmpty()
+    .withMessage("SubCategory name cannot be empty")
+    .trim()
+    .isLength({ min: 3, max: 32 })
+    .withMessage("Name must be between 3 and 32 characters"),
+  body("category")
+    .optional()
+    .notEmpty()
+    .withMessage("Category ID cannot be empty")
+    .isMongoId()
+    .withMessage("Invalid category ID format"),
+  validation,
+];
